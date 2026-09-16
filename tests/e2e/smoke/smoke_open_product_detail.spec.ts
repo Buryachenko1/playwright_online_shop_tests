6af923271dccac91d20475995da909f805315ae4
Jevgenija Burjačenko <98038623+Buryachenko1@users.noreply.github.com>
2026-09-16 14:53:01 +0200

    homepage products count check smoke added

--- tests/e2e/smoke/smoke_open_product_detail.spec.ts
+++ tests/e2e/smoke/smoke_open_product_detail.spec.ts
@@ -1,26 +1,21 @@
 import { test } from "@playwright/test";
 import { HomePage } from "../../../src/pages/presta/home_page";
 
-
-test("@smoke Product can be added to cart", async ({ page }) => {
+test("@smoke Product detail opens and displays basic information", async ({
+  page,
+}) => {
   const homePage = new HomePage(page);
 
-  await test.step("Open product detail), async () => {
-    await homePage
-      .openHomePage()
-
-      productName = await homePage.getProductName();
-      productPage = await homePage.openProductDetail();
-
-      await productPage.verifyProductPageIsVisible()
-  
+  await test.step("Open home page", async () => {
+    await homePage.openHomePage();
+    await homePage.verifyHomePageIsVisible();
   });
 
-  await test.step("Add product to cart", async () => {
-    await productPage.addProductToCart();
-  });
+  await test.step("Open product detail", async () => {
+    const productName = await homePage.getProductName();
+    const productPage = await homePage.openProductDetail();
 
-  await test.step("Verify product is in cart", async () => {
-    await productPage.verifyProductAddedToCart(productName);
+    await productPage.verifyProductPageIsVisible();
+    await productPage.verifyProductTitle(productName);
   });
 });
